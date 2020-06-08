a56f67c6ac589110b533c163b9a5f500ec0876f3
Syk Houdeib <syk.houdeib@gmail.com>
2020-06-08 19:26:36 +0200

    Updates

---
+++ precache-manifest.b507b9d93c515d2be123657aaf31b010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912866822c42bd33b088b0ec1a240ea4",
+    "url": "/reminder-app-redux-tutorial/index.html"
+  },
+  {
+    "revision": "8620164c60f942a31d72",
+    "url": "/reminder-app-redux-tutorial/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "27a199722d75b30eac70",
+    "url": "/reminder-app-redux-tutorial/static/js/2.bf6c4a3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reminder-app-redux-tutorial/static/js/2.bf6c4a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8620164c60f942a31d72",
+    "url": "/reminder-app-redux-tutorial/static/js/main.6b544777.chunk.js"
+  },
+  {
+    "revision": "49f96f438b0fc936f080",
+    "url": "/reminder-app-redux-tutorial/static/js/runtime-main.e8f0d6a0.js"
+  }
+]);
